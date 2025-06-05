9bf088931a24c381bff7fc75dfea1b2e72d2bd25
James Sue-Ling <james_sueling@hotmail.co.uk>
2025-06-05 17:39:59 +0100

    rename for clarity

--- load_best_weight_sample.py
+++ load_best_weight_sample.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def load_best_sample_with_metadata():
+def load_best_weight_sample():
     """Load the best sample with full metadata."""
     data = np.load('./out/best_sample_with_metadata.npy', allow_pickle=True).item()
     print(data)
 
 if __name__ == "__main__":
-    load_best_sample_with_metadata()
+    load_best_weight_sample()
